84ad23f3f398f9668d60b8af9926612fd214ae63
tianhanl <tianhanl@usc.edu>
2019-01-21 02:04:26 +0000

    adapt async structure from grpc example

--- store/key_value_store_server.cc
+++ store/key_value_store_server.cc
@@ -1,4 +1,153 @@
+#include <memory>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <grpcpp/grpcpp.h>
+#include <grpc/support/log.h>
+
+#include "./dist/key_value_store.grpc.pb.h"
+
+using chirp::KeyValueStore;
+using chirp::PutReply;
+using chirp::PutRequest;
+using grpc::Server;
+using grpc::ServerAsyncResponseWriter;
+using grpc::ServerBuilder;
+using grpc::ServerCompletionQueue;
+using grpc::ServerContext;
+using grpc::Status;
+
+class KeyValueStoreServerImpl final
+{
+public:
+  ~KeyValueStoreServerImpl()
+  {
+    server_->Shutdown();
+    cq_->Shutdown();
+  }
+
+  void Run()
+  {
+    std::string server_address("0.0.0.0:50000");
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service_" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *asynchronous* service.
+    builder.RegisterService(&service_);
+    // Get hold of the completion queue used for the asynchronous communication
+    // with the gRPC runtime.
+    cq_ = builder.AddCompletionQueue();
+    // Finally assemble the server.
+    server_ = builder.BuildAndStart();
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Proceed to the server's main loop.
+    HandleRpcs();
+  }
+
+private:
+  // Class encompasing the state and logic needed to serve a request.
+  class CallData
+  {
+  public:
+    // Take in the "service" instance (in this case representing an asynchronous
+    // server) and the completion queue "cq" used for asynchronous communication
+    // with the gRPC runtime.
+    CallData(KeyValueStore::AsyncService *service, ServerCompletionQueue *cq)
+        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE)
+    {
+      // Invoke the serving logic right away.
+      Proceed();
+    }
+
+    void Proceed()
+    {
+      if (status_ == CREATE)
+      {
+        // Make this instance progress to the PROCESS state.
+        status_ = PROCESS;
+
+        // As part of the initial CREATE state, we *request* that the system
+        // start processing SayHello requests. In this request, "this" acts are
+        // the tag uniquely identifying the request (so that different CallData
+        // instances can serve different requests concurrently), in this case
+        // the memory address of this CallData instance.
+        service_->Requestput(&ctx_, &request_, &responder_, cq_, cq_, this);
+      }
+      else if (status_ == PROCESS)
+      {
+        // Spawn a new CallData instance to serve new clients while we process
+        // the one for this CallData. The instance will deallocate itself as
+        // part of its FINISH state.
+        new CallData(service_, cq_);
+
+        // TODO: Put data
+
+        // And we are done! Let the gRPC runtime know we've finished, using the
+        // memory address of this instance as the uniquely identifying tag for
+        // the event.
+        status_ = FINISH;
+        responder_.Finish(reply_, Status::OK, this);
+      }
+      else
+      {
+        GPR_ASSERT(status_ == FINISH);
+        // Once in the FINISH state, deallocate ourselves (CallData).
+        delete this;
+      }
+    }
+
+  private:
+    // The means of communication with the gRPC runtime for an asynchronous
+    // server.
+    KeyValueStore::AsyncService *service_;
+    // The producer-consumer queue where for asynchronous server notifications.
+    ServerCompletionQueue *cq_;
+    // Context for the rpc, allowing to tweak aspects of it such as the use
+    // of compression, authentication, as well as to send metadata back to the
+    // client.
+    ServerContext ctx_;
+
+    // What we get from the client.
+    PutRequest request_;
+    // What we send back to the client.
+    PutReply reply_;
+
+    // The means to get back to the client.
+    ServerAsyncResponseWriter<PutReply> responder_;
+
+    // Let's implement a tiny state machine with the following states.
+    enum CallStatus
+    {
+      CREATE,
+      PROCESS,
+      FINISH
+    };
+    CallStatus status_; // The current serving state.
+  };
+
+  void HandleRpcs()
+  {
+    new CallData(&service_, cq_.get());
+    void *tag;
+    bool ok;
+    while (true)
+    {
+      GPR_ASSERT(cq_->Next(&tag, &ok));
+      GPR_ASSERT(ok);
+      static_cast<CallData *>(tag)->Proceed();
+    }
+  }
+  std::unique_ptr<ServerCompletionQueue> cq_;
+  KeyValueStore::AsyncService service_;
+  std::unique_ptr<Server> server_;
+};
+
 int main(int argc, char **argv)
 {
+  KeyValueStoreServerImpl server;
+  server.Run();
   return 0;
 }
