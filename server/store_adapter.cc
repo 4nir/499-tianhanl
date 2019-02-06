8d1b8aacc82e7226f19e107d97af15faa73fff02
tianhanl <tianhanl@usc.edu>
2019-02-06 07:11:44 +0000

    change the storage and retrival of thread keys using custom message RecordReply

--- server/store_adapter.cc
+++ server/store_adapter.cc
@@ -1,4 +1,5 @@
 #include "store_adapter.h"
+#include <iterator>
 
 void StoreAdapter::Init() { store_client_->Init(); }
 
@@ -24,8 +25,7 @@ UserInfo StoreAdapter::GetUserInfo(const std::string& username) {
 bool StoreAdapter::StoreChirp(const Chirp& chirp) {
   // If chirp is a reply, store it as reply to its parent
   if (chirp.parent_id() != "") {
-    std::lock_guard<std::mutex> guard(map_mutex_);
-    reply_map_[chirp.parent_id()] = chirp.id();
+    StoreReply(chirp.id(), chirp.parent_id());
   }
   std::string serialized_string;
   chirp.SerializeToString(&serialized_string);
@@ -47,21 +47,75 @@ Chirp StoreAdapter::GetChirp(const std::string& chirp_id) {
 
 std::vector<Chirp> StoreAdapter::GetChirpThread(const std::string& chirp_id) {
   std::vector<Chirp> chirps;
-  std::vector<std::string> keys = {chirp_id};
-
-  // Find IDs of replies to `chirp_id`, and push the IDs to keys
-  {
-    std::lock_guard<std::mutex> guard(map_mutex_);
-    auto search_result = reply_map_.find(chirp_id);
-    while (search_result != reply_map_.end()) {
-      keys.push_back(search_result->second);
-      search_result = reply_map_.find(search_result->second);
-    }
-  }
+  std::vector<std::string> keys = GetThreadKeys(chirp_id);
   store_client_->Get(keys, [&chirps](std::string value) {
     Chirp chirp;
     chirp.ParseFromString(value);
     chirps.push_back(chirp);
   });
   return chirps;
+}
+
+std::vector<std::string> StoreAdapter::GetReplyIds(const std::string& curr_id) {
+  std::vector<std::string> ids;
+
+  // Get ReplyRecord for curr_id
+  std::string key = "reply-" + curr_id;
+  std::vector<std::string> keys = {key};
+  std::string serialized_string;
+  store_client_->Get(keys, [&serialized_string](std::string value) {
+    serialized_string = value;
+  });
+
+  // Store each reply_id to ids if store has a record for curr_id
+  if (serialized_string != "") {
+    ReplyRecord reply_record;
+    reply_record.ParseFromString(serialized_string);
+    for (std::string id : reply_record.reply_ids()) {
+      ids.push_back(id);
+    }
+  }
+
+  return ids;
+}
+
+bool StoreAdapter::StoreReply(const std::string& curr_id,
+                              const std::string& parent_id) {
+  // Use `reply-` as a prefix to indicate it is key for `ReplyRecord`
+  std::string key = "reply-" + parent_id;
+
+  // Get previous `ReplyRecord` for this key
+  std::vector<std::string> keys = {key};
+  std::string serialized_string;
+  store_client_->Get(keys, [&serialized_string](std::string value) {
+    serialized_string = value;
+  });
+
+  // Create a empty ReplyRecord
+  ReplyRecord reply_record;
+  reply_record.set_id(parent_id);
+
+  // If previous record exists, restores its data
+  if (serialized_string != "") {
+    reply_record.ParseFromString(serialized_string);
+  }
+  reply_record.add_reply_ids(curr_id);
+
+  // Store updated ReplyRecord
+  reply_record.SerializeToString(&serialized_string);
+  return store_client_->Put(key, serialized_string);
+}
+
+std::vector<std::string> StoreAdapter::GetThreadKeys(
+    const std::string& chirp_id) {
+  std::vector<std::string> keys = {chirp_id};
+  int curr_pos = 0;
+  // Using queue to do pre-order traversal
+  while (curr_pos < keys.size()) {
+    std::vector<std::string> curr_keys = GetReplyIds(keys[curr_pos]);
+    keys.insert(keys.end(), std::make_move_iterator(curr_keys.begin()),
+                std::make_move_iterator(curr_keys.end()));
+    curr_pos += 1;
+  }
+  return keys;
 }
