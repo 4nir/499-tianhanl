4af430be368800e8450e91ee022bdabf2404d613
tianhanl <tianhanl@usc.edu>
2019-02-08 01:12:07 +0000

    add implementation of monitor in server

--- server/service_layer_server_sync.cc
+++ server/service_layer_server_sync.cc
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include "./dist/service_layer.grpc.pb.h"
@@ -127,9 +129,60 @@ class ServiceLayerServiceImpl final : public ServiceLayer::Service {
     }
     return Status::OK;
   }
+
   // Streams chirps from all followed users
   Status monitor(ServerContext* context, const MonitorRequest* request,
-                 ServerWriter<MonitorReply>* writer) {}
+                 ServerWriter<MonitorReply>* writer) {
+    UserInfo curr_user_info = store_adapter_->GetUserInfo(request->username());
+
+    // Starts monitoring
+    std::thread monitoring([writer, &curr_user_info, this]() {
+      std::vector<Chirp> chirps;
+      // Used to avoid duplicate
+      int latest_access_seconds = 0;
+      // Uses polling to check updates.
+      while (true) {
+        // Makr current time
+        auto mark_time = system_clock::now().time_since_epoch();
+        int mark_seconds = (duration_cast<seconds>(mark_time)).count();
+        std::vector<Chirp> chirps;
+        latest_access_seconds = std::max(latest_access_seconds, mark_seconds);
+        // wait 10 seconds
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        // If the users who current user is following has newer record,
+        // store the new chirp to chiprs
+        for (const std::string& username : curr_user_info.following()) {
+          UserInfo user_info = store_adapter_->GetUserInfo(username);
+          // If user record has been updated after mark_time
+          if (user_info.timestamp().seconds() >= mark_seconds) {
+            // Adds chirp posted after mark_time to chirps
+            for (int i = user_info.chirp_id_s_size() - 1; i >= 0; i--) {
+              Chirp chirp = store_adapter_->GetChirp(user_info.chirp_id_s(i));
+              // If the chirp is posted after mark_seconds, it is a new
+              // chirp
+              if (chirp.timestamp().seconds() >= mark_seconds) {
+                latest_access_seconds = std::max(
+                    latest_access_seconds, int(chirp.timestamp().seconds()));
+                chirps.push_back(chirp);
+              } else {
+                break;
+              }
+            }
+          }
+        }
+
+        // Now chirps have all chirps posted after mark time
+        for (Chirp chirp : chirps) {
+          MonitorReply reply;
+          CloneChirp(chirp, reply.mutable_chirp());
+          writer->Write(reply);
+        }
+      }
+    });
+    // Wait for monitoring to end
+    monitoring.join();
+    return Status::OK;
+  }
 
  private:
   // Creates a Timestamp object populated with current UNIX timestamp.
@@ -156,6 +209,7 @@ class ServiceLayerServiceImpl final : public ServiceLayer::Service {
     // Ownership of timestamp transfered to mutable_chirp
     mutable_chirp->set_allocated_timestamp(timestamp);
   }
+
   //  Interface to communicate with store server
   std::unique_ptr<StoreAdapter> store_adapter_;
 };
